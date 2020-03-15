ea4ef2aa13f222fae45075abc22806034e86424a
Matthew Herman <mattherman11@gmail.com>
2020-03-15 15:48:34 -0500

    Add a watch to the docker setup

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run --name mattherman.github.io --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:3.8.5 jekyll serve --watch --drafts
+docker run --rm --name mattherman.github.io --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:3.8.5 jekyll serve --watch --drafts
